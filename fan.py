20fd061cbf5b88498984ef82f89d9e6bd6524afb
zefto-123 <sobirovasror05@gmail.com>
2024-10-14 16:21:00 +0500

    first commit

---
+++ fan.py
@@ -0,0 +1,155 @@
+#t_yil =  int(input("yilingni kirit:"))
+#yosh =2024 - t_yil  #
+#print("siz"  +str(yosh)+ "yoshda ekansiz" )
+#meavalar = ['banan','olma','gilos',]
+#narx=[23000,4000,17000,]
+#sonlar=['bir','ikki',3,4,5]
+
+#meavalar=['banan','olma','gilos']
+#print("birinchi meva:", mavalar [1]
+#print("kfgnk")
+#cars = ['bmw','audi','mers','mustang','damas','lada vaz']
+#cars.sort(reverse=True)
+#print(cars)
+#ages=[12,98,101,4,52,48]
+#ages.sort()
+#print(ages)
+#print(sorted(ages,reverse=True))
+#juft_sonlar = list(range(0,41,2)) 
+#toq_sonlar = list(range(1,56,2))  
+#print("Juft sonlar: ", juft_sonlar)
+#print("Toq sonlar: ", toq_sonlar)
+#narhlar = [512000, 6500, 23456, 9800, 5600, 99999999, 32874]
+#arzon = min(narhlar)
+#qimmat = max(narhlar)
+#jami = sum(narhlar)
+#print("Eng arzon narh ", arzon, ". Eng qimmati ", qimmat, ". Jami: ", jami)
+#cars=['bmw','mersedes benz','volvo','gm','tesla']
+#my_cars=cars[0:2]
+#print(my_cars)
+#toys=('bus','car','lizart','sanke')
+#print(toys[0])
+#print(toys[-1])
+#print(toys[2:5])
+#davlatlar=['aqsh','russia','germaniya','brazilya','ispania']
+#davlatlar.sort(reverse=True)
+#print(davlatlar)
+#narhlar = [2500,2600,2700,500,2800,2900,3000]
+#arzon = min(narhlar)
+#qimmat = max(narhlar)
+#jami = sum(narhlar)
+#print("Eng arzon narh ", arzon, ". Eng qimmati ", qimmat, ". Jami: ", jami)
+#taomlar=['osh','norin','somsa','shashlik','tuxum']
+#nonushta=taomlar[:]
+#nonushta.remove('norin')
+#nonushta.remove('osh')
+#nonushta.remove('somsa')
+# nonushta.append('nonva choy ')
+#dostlar = [] 
+#print("5 ta eng yaqin do'stingiz kim?")
+#for n in range(5):
+#   dostlar.append(input(f"{n+1}-do'stingizning ismini kiriting: "))
+#print(dostlar)
+#sonlar=list(range(1,11))
+#for son in sonlar:
+ #       print(f"{son}ning kvadrati {son**2} ga teng
+#mehmonlar = ['Ali','Vali','Mansur', 'Husan','Murad']
+#for mehmon in mehmonlar:
+#    print(mehmon)
+#    
+#    print(mehmonlar) 
+#mehmonlar=['Ali','Vali','Murod']
+#for mehmon in mehmonlar:
+#    print(f"Hurmatli  {mehmon} ,sizni 31-fevral kuini soat 26:00da nahorga shashlikka taklif qilamiz")
+#print("hurmat bilan    Z E F T O")
+#sonlar = list(range(120,1200,2))
+#print(max(sonlar)-min(sonlar))
+#mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
+#for mehmon in mehmonlar:
+#    print(f"salom {mehmon}"
+#sonlar=list(range(11,100,2))
+#for son in sonlar:
+#    print(son)
+#    print(son**3)
+#dostlar = [] 
+#print("5 ta eng yaqin do'stingiz kim?")
+#for n in range(5): 
+#    dostlar.append(input(f"{n+1}-Suhbat qilgan odaminggiz kim edi: "))
+#print(dostlar)
+#avtolar = ['audi','bmw','gm','kia','hyundai']
+#for avto in avtolar: 
+#    if avto == 'gm':  
+#        print(avto.upper()) 
+#    else: 
+#        print(avto.title())
+#ism = input('Ismingiz nima?\n>>>')
+#if ism.lower() != 'zefto': 
+#    print(f"Uzr, {ism.title()} bizga zefto kerak") 
+#else:
+#    print("Salom, zefto")
+
+#ism= input('ismingiz nima?\n>>>>')
+#if ism.lower() != 'admin' :
+#    print(" uzr,zefto ")
+#else:
+#    print("xush kelibsiz")
+#son = float(input (hohlagan son:))
+#print("manfiy son")if son < 0 else print("musbat son")
+#
+#sonlar=list(range(0,10))
+# print(sonlar)
+# yosh =int(input('yoshingiz nechida'))
+# if yosh<=25:
+#     print('sizga kirish bepul.')
+# elif yosh<=26:
+#     print('sizga kirirsh 5000so\'m')
+# else:
+#     print('sizga kirish 100000 so\'m')
+# yosh=int(input('yoshingiz nechada'))
+# if yosh<=20:
+#     price =0
+# elif yosh<=40:
+#     price=500000
+# else:
+#     price=100000
+# print(f"sizga kirish {price} so`m")
+# kun=input('bugun nima kun?>>>>')
+# if kun.lower()=='shanba' or kun.lower()=='yakshanba':
+#     print('bugun damolish kunni.')
+# else:
+#     print('bugun ish kuni')
+# kun = input("Bugun nima kun?")
+# harorat = float(input("Havo harorati qanday?"))
+# if kun.lower()=='yakshanba' and harorat>=30:
+#     print("Cho'milgani ketdik!")
+# elif kun.lower()=='yakshanba' and harorat<50:
+#     print("Uyda dam olamiz!")
+# son = [12,5,14,13,9,8,78,589]
+# son.sort(10)
+# print(son)
+# print(sorted(son, reverse=10))
+
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz','kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+# savat = []
+# for n in range(5):
+#     savat.append('input('f"Savatga {n+1}-mahsulotni qoshing:))
+# # bor_mahsulotlar = []
+# # mavjud_emas = []
+# # for mahsulot in savat:
+# # if mahsulot in mahsulotlar:
+# #     print(bor_mahsulotlar.append(mahslot))
+# # else:
+# #     print(mavjud_emas.append(mahsulot))
+
+# if mavjud_emas:
+#   print("Do'konimizda quyidagi mahsulotlar yo'q:")
+# for mahsulot in mavjud_emas:
+#   print(mahsulot)
+# else:
+#   print ("Siz so'ragan barcha mahsulotlar do'konimizda bor":)"""
+# 27/11/2020
+# Dasturlash asoslari
+# #12-dars: Xatolar
+# Muallif: Anvar Narzullaev
+# Web sahifa: https://python.sariq.dev
+# """
